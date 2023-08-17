ba66194193276443b89d0531e58b0738eafa8069
Ferid Amir <93754712+nubiemir@users.noreply.github.com>
2023-08-17 23:23:41 +0400

    Update Row.js

--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,26 @@
+
 import Selector from "./Selector";
 import Input from "./Input";
 import Button from "./Button";
+import { styled } from "styled-components";
 
-const Row = ()=> {
+const Row = ({data, handleDelete, handleDisabled, handleInputChange, handleSelectorChange})=> {
     return (
-        <>
-            <Selector/>
-            <Input/>
-            <Button secondary text="Add Row" func={()=> console.log("HI")} />
-            <Button warning text="Add Row" func={()=> console.log("HI")} />
-        </>
+        <RowStyle>
+            <Selector id = {data.id} value = {data.value} setValue={handleSelectorChange}/>
+            <Input id = {data.id} value = {data.value} setValue={handleInputChange}/>
+            {data.disabled ? 
+            <Button tertiary text="Enable" func={handleDisabled} /> :
+            <Button secondary text="Disable" func={handleDisabled} /> }
+            <Button warning text="Delete" func={handleDelete} />
+        </RowStyle>
     )
 }
+
+
+const RowStyle = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
 
 export default Row;
