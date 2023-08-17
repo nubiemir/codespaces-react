c1d80507e445860c56032a61ca7e85b0954e9f8e
Ferid Amir <93754712+nubiemir@users.noreply.github.com>
2023-08-17 23:25:07 +0400

    Update Button.js

--- src/Button.js
+++ src/Button.js
@@ -10,9 +10,9 @@ const Button = ({text, func, ...rest})=> {
 const BtnStyle = styled.button`
     padding: 0.5rem 1rem;
     border: 0;
-    border: 1px solid ${props => props.primary ? "#18e014" : props.secondary ? "#f55b60" : ""};
-    color: ${props => props.primary ? "#18e014" : props.warning ? "#ffffff" : props.secondary ? "#f55b60" : ""};
-    background: ${props => props.warning ? "#f55b60" : "#ffffff"};
+    border: 1px solid ${props => props.primary ? "#18e014" : props.secondary ? "#f55b60" : props.tertiary ? "#18e014" :"#f55b60"};
+    color: ${props => props.primary ? "#18e014" : props.warning || props.tertiary ? "#ffffff" : props.secondary ? "#f55b60" : ""};
+    background: ${props => props.warning ? "#f55b60" :props.tertiary ? "#18e014" : "#ffffff"};
     border-radius: 5px;
     cursor: pointer;
     
